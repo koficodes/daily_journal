e7cc5e2425fb92bcb608bf7ddf872729d0b76819
paul karikari <paulkarikari1@gmail.com>
2018-04-05 18:08:44 +0000

    convert views to class based views

--- journal/urls.py
+++ journal/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_journals, name='list-journals'),
-    path('add-journal', views.add_journal, name='add-journal'),
-    path('<int:pk>/show-journal', views.show_journal, name='show-journal'),
-    path('<int:pk>/edit-journal', views.edit_journal, name='edit-journal'),
-    path('<int:pk>/delete-journal', views.delete_journal, name='delete-journal')
+    path('', views.JournalListView.as_view(), name='list-journals'),
+    path('add-journal', views.JournalCreateView.as_view(), name='add-journal'),
+    path('<int:pk>/show-journal',
+         views.JournalDetailView.as_view(), name='show-journal'),
+    path('<int:pk>/edit-journal',
+         views.JournalUpdateView.as_view(), name='edit-journal'),
+    path('<int:pk>/delete-journal',
+         views.JournalDeleteView.as_view(), name='delete-journal')
 ]
--- journal/views.py
+++ journal/views.py
@@ -1,57 +1,35 @@
-from django.shortcuts import render, redirect, get_object_or_404
-# redirect and get_object_or_404 modules will be used later.
 from .models import Journal
-from .forms import JournalFoam
+from django.urls import reverse_lazy
+from django.views.generic import (ListView, CreateView, DetailView, 
+                                  UpdateView, DeleteView)
 
 
-def list_journals(request):
-    context = {'journals': Journal.objects.all}
-    return render(request, 'journal/list_journals.html', context)
+class JournalListView(ListView):
+    model = Journal
+    template_name = 'journal/list_journals.html'
+    context_object_name = 'journals'
 
-def add_journal(request):
 
-    if request.method == 'POST':
-        form = JournalFoam(request.POST)
-        if form.is_valid():
-            title = form.cleaned_data['title']
-            content = form.cleaned_data['content']
+class JournalCreateView(CreateView):
+    model = Journal
+    template_name = "journal/add_journal.html"
+    fields = ['title', 'content']
 
-            journal = Journal.objects.create(title=title, content=content) # assign to a journal variable
 
-            return redirect(journal.get_absolute_url()) 
-
-    context = {'form': JournalFoam()}
-    return render(request, 'journal/add_journal.html', context)
-
-def show_journal(request, pk):
-    journal = get_object_or_404(Journal, pk=pk)
-    return render(request, 'journal/show_journal.html', {'journal': journal} )
+class JournalDetailView(DetailView):
+    model = Journal
+    template_name = 'journal/show_journal.html'
+    context_object_name = 'journal'
     
 
-def edit_journal(request, pk):
-    journal = get_object_or_404(Journal, pk=pk)
+class JournalUpdateView(UpdateView):
+    model = Journal
+    context_object_name = 'journal'
+    template_name = 'journal/edit_journal.html'
+    fields = ['title', 'content']
 
-    if request.method == 'POST':
-        form = JournalFoam(request.POST)
-        if form.is_valid():
-            journal.title = form.cleaned_data['title']
-            journal.content = form.cleaned_data['content']
 
-            journal.save()
-            return redirect('list-journals')
-
-    form = JournalFoam(
-        initial={'title': journal.title, 'content': journal.content})
-
-    return render(request, 'journal/edit_journal.html',
-                  {'form': form, 'pk': pk})
-
-def delete_journal(request, pk):
-    journal = get_object_or_404(Journal, pk=pk)
-
-    if request.method == 'POST':
-        journal.delete()
-        return redirect('list-journals')
-
-    return render(request, 'journal/journal_confirm_delete.html',
-                  {'journal': journal})
+class JournalDeleteView(DeleteView):
+    model = Journal
+    context_object_name = 'journal'
+    success_url = reverse_lazy('list-journals')
